17dad5fdc123d4366fca40fc03ddc6e98787df8a
farhanattarii <160287092+farhanattarii@users.noreply.github.com>
2024-05-23 08:51:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+/*** Question # 01 ***/
+var fruits = ["apple", "banana", "mango", "orange"];
+//  console.log(fruits);
+/*** Question # 02 ***/
+// let numbers : number[] = [10, 20, 30, 40];
+// console.log(number);
+/*** Question # 03 ***/
+// let thirdFruit : string= fruits[3]
+// console.log(thirdFruit);
+/*** Question # 04 ***/
+// numbers[2] = 25;
+// console.log(numbers);
+/*** Question # 05 ***/
+// fruits.push("grape");
+// console.log(fruits);
+/*** Question # 06 ***/
+// let lastFruit = fruits.pop();
+// console.log(lastFruit);
+// console.log(fruits);
+/*** Question # 07 ***/
+// let firstFruit = fruits.shift();
+// console.log(firstFruit);
+// console.log(fruits);
+/*** Question # 08 ***/
+// fruits.unshift("Kiwi");
+// console.log(fruits);
+/*** Question # 09 ***/
+// fruits.splice(1 , 2)
+// console.log(fruits);
+/*** Question # 10 ***/
+// fruits.splice(2 , 0, "pineapple", "pear");
+// console.log(fruits);
+/*** Question # 11 ***/
+// let citrusFruits = fruits.slice(0 , 2);
+// console.log(citrusFruits);
+// console.log(fruits);
+/*** Question # 12 ***/
+var lastTwoFruit = fruits.slice(-2);
+console.log(lastTwoFruit);
